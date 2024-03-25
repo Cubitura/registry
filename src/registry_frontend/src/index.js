8f8bd4862e464ce3c77fda2cc368ac05b7cd2a6e
carstenjacobsen <carstenjacobsen@Carstens-MacBook-Pro.local>
2024-03-25 00:30:50 -0700

    Adding registry and demo

---
+++ src/registry_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { registry_backend } from "../../declarations/registry_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await registry_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
